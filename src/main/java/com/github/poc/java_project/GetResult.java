6ab9d980145ed17dffcb10f86f9b66422c795116
fayaz-bahamany <140581005+fayaz-bahamany@users.noreply.github.com>
2023-09-01 16:07:56 +0400

    KAN-1 Initial commit new repository

---
+++ src/main/java/com/github/poc/java_project/GetResult.java
@@ -0,0 +1,14 @@
+
+public class GetResult  
+{  
+public static int add(int i, int j)  
+{ 
+
+int sum = 0;
+sum = i + j;
+    System.out.println("The sum of the numbers is: "+sum);  
+	System.out.println("THis is one more print statement");
+return sum;
+  
+}  
+}  
---
+++ src/test/java/com/github/poc/java_project/JavaProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.poc.java_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
