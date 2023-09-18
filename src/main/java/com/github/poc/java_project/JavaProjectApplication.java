de1f79f9614e07e0631ad05eef318312fd5ba626
fayaz-bahamany <140581005+fayaz-bahamany@users.noreply.github.com>
2023-09-18 14:19:37 +0400

    GJP-1 #comment Update JavaProjectApplication.java
    GJP-1 #comment Update JavaProjectApplication.java

--- src/main/java/com/github/poc/java_project/JavaProjectApplication.java
+++ src/main/java/com/github/poc/java_project/JavaProjectApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+
+//Commented	
 public class JavaProjectApplication {
 
 	//Commented this code
